89792e5f80abed02084565afd2551bb1454c382b
bensilke-amd <ben.silke@aristotlemetadata.com>
2022-08-02 17:59:25 +1000

    added demo config

---
+++ src/myproject/__init__.py
@@ -0,0 +1,10 @@
+"""my doctsring
+"""
+__author__ = "Ben Silke"
+__copyright__ = "Copyright 2016-2021, Ben Silke"
+__credits__ = ["Ben Silke"]
+__license__ = "BSD"
+__version__ = "2022.8"  # A DATE BASED VERSION
+__maintainer__ = "Ben Silke"
+__email__ = "u6675274@anu.edu.au"
+__status__ = "alpha"
