ee5afa6884bfcf5f9af8d0d8b92fd8d840160bcc
bensilke-amd <ben.silke@aristotlemetadata.com>
2022-08-17 19:20:19 +1000

    parser additons

--- src/genetifinder/parser.py
+++ src/genetifinder/parser.py
@@ -1,6 +1,6 @@
 import os
 from parsers.genbank_parser import GenbankParser
-from cogent3.parse.genbank import MinimalGenbankParser
+from cogent3.parse.genbank import MinimalGenbankParser, RichGenbankParser
 
 
 os.chdir("/Users/bensilke/gavins-lab/biol3209/")
@@ -14,33 +14,43 @@ files = ["data/soil_reference_genomes/NC_000913.3.gb"]
 genes = []
 for file in files:
     with open(file) as f:
-        parser = MinimalGenbankParser(f)
+        # parser = MinimalGenbankParser(f)
+        parser = RichGenbankParser(f)
         for p in parser:
             genes.append(p)
 
-refs = []
-ref_dict = {}
-no_refs = []
+gene = genes[0]
+print(gene)
+print(type(gene))
+print(type(gene[1]))
+seq = gene[1]
 
-for gene in genes:
-    accession = gene["accession"]
-    ref_dict[accession]
-    for feature in gene["features"]:
-        # convert then make a csv of converted data
-        g = {
-            "type": feature.get("type", ""),
-            "gene_synonym": feature.get("gene_synonym", ""),
-            "gene": feature.get("gene", ""),
-            "locus_tag": feature.get("locus_tag", ""),
-            "db_xref": feature.get("db_xref", []),
-        }
-        ref_dict[accession][g["gene"]] = g
-        if g["db_xref"]:
-            refs.extend(feature["db_xref"])
-        else:
-            no_refs = {}
-            ref_dict[accession][g["gene"]] = None
 
-print(refs)
-print(set(refs))
-print(f"{ref_dict=}")
+print(seq.protein)
+print(dir(seq))
+# refs = []
+# ref_dict = {}
+# no_refs = []
+
+# for gene in genes:
+#     accession = gene["accession"]
+#     ref_dict[accession]
+#     for feature in gene["features"]:
+#         # convert then make a csv of converted data
+#         g = {
+#             "type": feature.get("type", ""),
+#             "gene_synonym": feature.get("gene_synonym", ""),
+#             "gene": feature.get("gene", ""),
+#             "locus_tag": feature.get("locus_tag", ""),
+#             "db_xref": feature.get("db_xref", []),
+#         }
+#         ref_dict[accession][g["gene"]] = g
+#         if g["db_xref"]:
+#             refs.extend(feature["db_xref"])
+#         else:
+#             no_refs.extend(accession)
+#             ref_dict[accession][g["gene"]] = None
+
+# print(refs)
+# print(set(refs))
+# print(f"{ref_dict=}")
