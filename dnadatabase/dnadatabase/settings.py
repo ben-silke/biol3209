e5212d14e860dc0f2a53e32123ecd8bfa8c8672e
bensilke-amd <ben.silke@aristotlemetadata.com>
2022-08-18 16:15:25 +1000

    fixed secret key

--- dnadatabase/dnadatabase/settings.py
+++ dnadatabase/dnadatabase/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ckoz$g2l#c+i=^_*u#v=p3ojndyxly72piuf%nho_$e5+=5+8q"
+from secret_key import SECRET_KEY
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
